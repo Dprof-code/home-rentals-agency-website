28c2dc3dc8f87395f1c3b168a8b81a5a5762cfcc
Dprof-Code <olawaleade15@gmail.com>
2023-01-20 13:42:50 -0600

    styled the review section

--- files/js/script.js
+++ files/js/script.js
@@ -1,1 +1,8 @@
 console.log("script loaded!");
+
+let fileSelectBtn = document.getElementById("file-select");
+let fileInput = document.getElementById("file-input");
+
+fileSelectBtn.addEventListener("click", function click() {
+  fileInput.click();
+});
